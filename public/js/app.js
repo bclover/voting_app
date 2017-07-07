0604bbd0c4ec08f7275e7b6da883809468cc6864
Bryan Clover <bclover09@gmail.com>
2017-07-07 09:50:04 -0400

    refactor helper methods to use arrow functions

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,17 @@
 class ProductList extends React.Component {
+  state = {
+    products: [],
+  };
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: [],
-    };
-
-    this.handleProductUpVote = this.handleProductUpVote.bind(this);
+  componentDidMount(){
+    this.setState({products: Seed.products});
   }
 
-  componentDidMount() {
-    this.setState({ products: Seed.products });
-  }
-
-  handleProductUpVote(productId){
-    const nextProducts = this.state.products.map((product) => {
+  handleProductUpVote = (productId) =>{
+    const nextProducts = this.state.products.map((product) =>{
       if(product.id === productId){
         return Object.assign({}, product, {
-          votes: product.votes + 1,
+          votes: product.votes+1,
         });
       }else{
         return product;
@@ -28,12 +21,12 @@ class ProductList extends React.Component {
     this.setState({
       products: nextProducts
     });
-  }
+  };
 
   render(){
 
     const products = this.state.products.sort((a, b) => (
-      b.votes - a.votes
+      b.votes-a.votes
     ));
 
     const productComponents = products.map((product) => (
@@ -58,14 +51,9 @@ class ProductList extends React.Component {
 
 class Product extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.handleUpVote = this.handleUpVote.bind(this);
-  }
-
-  handleUpVote(){
+  handleUpVote = () => (
     this.props.onVote(this.props.id)
-  }
+  );
 
   render(){
     return (
